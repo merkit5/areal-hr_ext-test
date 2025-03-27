1b29eb128b7440c31ef623d6663e453019892704
merkit5 <papaevmix@mail.ru>
2025-03-27 16:12:48 +0300

    added migrations for organization, department, position

---
+++ api/migrations/1743080207296_create-positions-table.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('position', {
+        id: {
+            type: 'serial',
+            primaryKey: true
+        },
+        name: {
+            type: 'varchar(255)',
+            notNull: true
+        },
+        created_at: {
+            type: 'timestamp',
+            default: pgm.func('current_timestamp')
+        },
+        updated_at: {
+            type: 'timestamp',
+            default: pgm.func('current_timestamp')
+        },
+        deleted_at: {
+            type: 'timestamp',
+            default: null
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('position');
+};
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('config/db');
-require('dotenv').config({path: '../.env'});
+const pool = require('./config/db');
+require('dotenv').config({path: './.env'});
 
 const app = express();
 
